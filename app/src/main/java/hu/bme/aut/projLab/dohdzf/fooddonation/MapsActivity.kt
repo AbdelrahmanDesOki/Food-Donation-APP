ec20fb5f55d8eeed6953acdd51fc83564aa2621c
Desokii <desoki.abdelrahman@gmail.com>
2023-04-29 22:56:21 +0200

    Fixing Marker on the map and showing exact location

--- app/src/main/java/hu/bme/aut/projLab/dohdzf/fooddonation/MapsActivity.kt
+++ app/src/main/java/hu/bme/aut/projLab/dohdzf/fooddonation/MapsActivity.kt
@@ -1,6 +1,7 @@
 package hu.bme.aut.projLab.dohdzf.fooddonation
 
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,17 +15,21 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import hu.bme.aut.projLab.dohdzf.fooddonation.databinding.ActivityMapsBinding
+import java.util.*
 
+
+//Need to fix how to move marker on map
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
-
+    private lateinit var mMap : GoogleMap
     private lateinit var currentlocation: Location
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
+    var currentMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,17 +50,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
       return
     }
 
-    val getLocation = fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-      location ->
+    val getLocation = fusedLocationProviderClient?.lastLocation
 
+      getLocation?.addOnSuccessListener {
+      location ->
       if(location != null ){
-        currentlocation = location
+        this.currentlocation = location
 
         Toast.makeText(applicationContext, currentlocation.latitude.toString() + " " + currentlocation.longitude.toString(), Toast.LENGTH_LONG ).show()
 
         val mapFragment = supportFragmentManager
           .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
       }
     }
 
@@ -75,15 +82,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
   }
 
     override fun onMapReady(googleMap: GoogleMap) {
+     mMap = googleMap
 
       val latlng = LatLng(currentlocation.latitude,  currentlocation.longitude)
-      val markerOptions= MarkerOptions().position(latlng).title("Current Location")
+      drawMarker(latlng)
 
-      googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latlng))
-      googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 7f))
-      googleMap?.addMarker(markerOptions)
+      mMap.setOnMarkerDragListener(object: GoogleMap.OnMarkerDragListener{
+
+        override fun onMarkerDrag(p0: Marker) {
+
+        }
+
+        override fun onMarkerDragEnd(p0: Marker) {
+           if(currentMarker!=null){
+             currentMarker?.remove()
+
+             val newLatLng = LatLng(p0?.position!!.latitude, p0?.position!!.longitude)
+             drawMarker(newLatLng)
+           }
+        }
+
+        override fun onMarkerDragStart(p0: Marker) {
+
+        }
+      } )
 
 
 
     }
+
+  private fun drawMarker(latlng: LatLng){
+    val markerOptions = MarkerOptions().position(latlng).title("Current Location").snippet(getAddress(latlng.latitude, latlng.longitude)).draggable(true)
+          mMap?.animateCamera(CameraUpdateFactory.newLatLng(latlng))
+          mMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15f))
+          currentMarker =  mMap?.addMarker(markerOptions)
+          currentMarker?.showInfoWindow()
+  }
+
+  private fun getAddress(lat: Double, lon: Double) : String?  {
+   val geocoder = Geocoder(this, Locale.getDefault())
+    val address =  geocoder.getFromLocation(lat,lon, 1)
+    return address[0].getAddressLine(0).toString()
+  }
 }
